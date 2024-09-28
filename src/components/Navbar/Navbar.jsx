c05d36ff04002d40b89c8eb51ca2ac0346a525b4
YahyaJohn0 <yk5550950@gmail.com>
2024-09-28 08:44:40 -0700

    commit navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import logo from '../../assets/logo.png'
 import profile from '../../assets/profile_img.png'
-import search_icon from '../../assets/search_icon.svg'
+import bell_icon from '../../assets/bell_icon.svg'
 import './Navbar.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import {faBellConcierge} from '@fortawesome/free-solid-svg-icons'
+import {faCaretDown} from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
   return (
@@ -21,9 +23,14 @@ const Navbar = () => {
             </ul>
         </div>
         <div className="navbar-right">
-        <FontAwesomeIcon icon={faMagnifyingGlass} />
+        <FontAwesomeIcon icon={faMagnifyingGlass} className='searchicon'/>
+        <FontAwesomeIcon icon={faBellConcierge} />
           <p>Kids</p>
-        <img src={profile} alt="" />
+          <div className="profile-icon">
+          <img src={profile} alt="" className='profile' />
+          <FontAwesomeIcon icon={faCaretDown} className='dropdown' />
+           <p className='dropText'><a href="#">SignOut</a></p>
+          </div>
         </div>
       </div>
     </div>
