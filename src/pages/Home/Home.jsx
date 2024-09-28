3bf5b8beb39e449a74bbad08f18552bfd17b7e65
YahyaJohn0 <yk5550950@gmail.com>
2024-09-28 11:44:18 -0700

    new changes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
+import Hero_banner from '../../assets/hero_banner.jpg'
+import Hero_title from '../../assets/hero_title.png'
+import play_icon from '../../assets/play_icon.png'
+import info_icon from '../../assets/info_icon.png'
+import './Home.css'
 const Home = () => {
   return (
-    <div>
+    <div className='home'>
       <Navbar/>
+      <div className="hero-banner">
+        <img src={Hero_banner} alt="" className='bannerimg'/>
+        <div className="hero-title">
+          <img src={Hero_title} alt="" className='titleimage'/>
+          <p>When a person want to make their own netflix intro,<br />
+          they find a web-based video editor to change their life forever</p>
+          <button className='playbtn'><img src={play_icon} alt="" />Play</button>
+          <button className='infobtn'><img src={info_icon} alt="" />MoreInfo</button>
+        </div>
+      </div>
+
     </div>
   )
 }
