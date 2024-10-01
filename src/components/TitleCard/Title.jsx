6a9f077f3039743735a531abff887dfe9b83c479
YahyaJohn0 <yk5550950@gmail.com>
2024-10-01 11:38:45 -0700

    all changes

--- src/components/TitleCard/Title.jsx
+++ src/components/TitleCard/Title.jsx
@@ -1,18 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import cards from '../../assets/cards/Cards_data'
 
 import './Title.css'
 
 const Title = () => {
+
+  const [apiData,setApiData] = useState([]);
+  
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNzQyOTc1M2E0MGI2NjQ0YTZmMmYxNTg2NjhkMzAyMyIsIm5iZiI6MTcyNzgwNjEwNy4yNzg3OSwic3ViIjoiNjZmYzM0ZTliZThhZDE4N2Q1YTY5NDY5Iiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.oC-WD-HkK_Tc_Www_kaSQarIvA54oHwx0rnIny1zv7I'
+    }
+  };
+  
+ 
+    useEffect(()=>{
+      fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
+      .then(response => response.json())
+      .then(response => setApiData(response.results))
+      .catch(err => console.error(err));
+    },[])
   return (
     <div>
         <div className="Cards-title">
             <h2>Popular Movies</h2>
         </div>
         <div className="card-list">
-            {cards.map((data,index)=>{
-                return <div className="cards" key={index}> <img src={data.image} alt="" />
-                <p >{data.name}</p>
+            {apiData.map((data,index)=>{
+                return <div className="cards" key={index}> <img src={`https://image.tmdb.org/t/p/w500`+data.backdrop_path} alt="" />
+                <p >{data.original_title}</p>
                 </div>
             })}
         </div>
